9c0bd5a4dcf3bf1e9e29a9ddeea4b64930168d46
zarinzaidi <aereinzayed@yahoo.com>
2024-05-12 10:11:58 +0800

    Exercise 3 submission'

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,17 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
-  };
+  apiKey: "AIzaSyDhJ5LZU8_6WAWq52n59nBQ4TicF58MnaQ",
+  authDomain: "fir-react-ex3.firebaseapp.com",
+  projectId: "fir-react-ex3",
+  storageBucket: "fir-react-ex3.appspot.com",
+  messagingSenderId: "758569326731",
+  appId: "1:758569326731:web:4e650930b899bc413e5c8e",
+  measurementId: "G-YVC5L6PYXF"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
-
-
 
 export { app, auth };
